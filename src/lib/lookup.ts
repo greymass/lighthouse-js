3d894e8a2cdf96c20d915ee4cfa4fafe43ac07aa
dafuga <danielfugere28@gmail.com>
2025-01-20 14:28:18 -0800

    fix: getting tests passing

---
+++ src/lib/lookup.ts
@@ -0,0 +1,44 @@
+import {APIClient, API, PublicKey} from '@wharfkit/antelope'
+import type {PermissionLevelType} from '@wharfkit/antelope'
+import type {Chain} from './chains'
+
+export const networkRequest = (
+	publicKey: PublicKey,
+	chain: Chain,
+	apiClient?: APIClient
+): Promise<PermissionLevelType[]> => {
+	return new Promise((resolve, reject) => {
+		const client = apiClient || new APIClient(chain)
+		const timeoutId = setTimeout(() => {
+			reject(new Error('Request timed out.'))
+		}, 1000)
+
+		client.v1.chain
+			.get_accounts_by_authorizers({
+				keys: [publicKey],
+			})
+			.then((response: API.v1.AccountsByAuthorizers) => {
+				clearTimeout(timeoutId)
+				resolve(
+					response.accounts.map((account) => ({
+						actor: account.account_name,
+						permission: account.permission_name,
+					}))
+				)
+			})
+			.catch((error) => {
+				clearTimeout(timeoutId)
+				reject(error)
+			})
+	})
+}
+
+export const lookupNetwork = async (publicKey: PublicKey, chain: Chain, apiClient?: APIClient) => {
+	try {
+		const accounts = await networkRequest(publicKey, chain, apiClient)
+		return {chain, accounts}
+	} catch (error) {
+		console.warn(`Lookup error on ${chain.name}: ${error}`)
+		return {chain, accounts: []}
+	}
+}
--- src/routes/lookup/[publicKey]/+server.ts
+++ src/routes/lookup/[publicKey]/+server.ts
@@ -1,46 +1,9 @@
-import {API, APIClient, type PermissionLevelType, PublicKey} from '@wharfkit/antelope'
+import {PublicKey} from '@wharfkit/antelope'
 import {MAINNET_CHAINS, TESTNET_CHAINS} from '$lib/chains'
 import type {Chain} from '$lib/types'
 import type {RequestEvent} from '@sveltejs/kit'
 import {error, json} from '@sveltejs/kit'
-
-const networkRequest = (
-	publicKey: PublicKey,
-	chain: Chain,
-	apiClient?: APIClient
-): Promise<PermissionLevelType[]> => {
-	return new Promise((resolve, reject) => {
-		const client = apiClient || new APIClient(chain)
-		client.v1.chain
-			.get_accounts_by_authorizers({
-				keys: [publicKey],
-			})
-			.then((response: API.v1.AccountsByAuthorizers) => {
-				resolve(
-					response.accounts.map((account) => ({
-						actor: account.account_name,
-						permission: account.permission_name,
-					}))
-				)
-			})
-			.catch((error) => {
-				reject(error)
-			})
-		setTimeout(() => {
-			reject('Request timed out.')
-		}, 1000)
-	})
-}
-
-const lookupNetwork = async (publicKey: PublicKey, chain: Chain, apiClient?: APIClient) => {
-	try {
-		const accounts = await networkRequest(publicKey, chain, apiClient)
-		return {chain, accounts}
-	} catch (error) {
-		console.warn(`Lookup error on ${chain.name}: ${error}`)
-		return {chain, accounts: []}
-	}
-}
+import {lookupNetwork} from '$lib/lookup'
 
 export const GET = async ({params, url}: RequestEvent) => {
 	const {publicKey} = params
